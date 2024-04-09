65b20b21657763ef1f13d64fcde2ca7aba4a7c25
jivjava2203 <jivjava2203@gmail.com>
2024-04-09 14:32:54 +0800

    added content on the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,14 @@
 export const About = () => {
   return (
     <section>
-        Hello World!
+      <div id="about">
+        <div className="abt-title">
+        <h1>About</h1>
+        </div>
+        <div className="about-content">
+        <p>The website provides a compilation of common Matigsalug words alongside their English translations. Its primary objective is to preserve and promote the Matigsalug language. In an era where linguistic diversity faces threats from global influences, this platform aims to safeguard indigenous languages like Matigsalug, ensuring their cultural richness endures. By offering accessible Matigsalug vocabulary, the website encourages language learning and fosters a deeper appreciation for indigenous heritage. Its mission is to contribute to the preservation and revitalization of the Matigsalug language for generations to come.</p>
+        </div>
+      </div>
     </section>
   )
 }
